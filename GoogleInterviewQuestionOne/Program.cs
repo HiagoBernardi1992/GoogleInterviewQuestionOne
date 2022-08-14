b8eb67827951a35ff865ec5daef9476b45157e1f
Hiago Bernardi <hiago.bernardi@gmail.com>
2022-08-14 09:20:44 +0100

    explain

--- GoogleInterviewQuestionOne/Program.cs
+++ GoogleInterviewQuestionOne/Program.cs
@@ -43,41 +43,55 @@ namespace GoogleInterviewQuestionOne
 
             int decompress(String input, int start, StringBuilder sb)
             {
+                //variable to know how many times I have to iterate the string who comes
                 StringBuilder times = new StringBuilder();
+                //variable to storage the string that I have to print by the times
                 StringBuilder current = new StringBuilder();
 
                 int i = start;
                 while (i < input.Length)
                 {
+                    //if the digir of the string is a number
                     while (Char.IsDigit(input[i]))
                     {
+                        //I will concat it untilI get all the characters of the number
                         times.Append(input[i]);
                         i++;
                     }
 
+                    //given the interation
+                    //if is [ is becausa I know that I previous storage the number
+                    //I also check the length for string inptu like that "["
                     if (input[i] == '[' && times.Length > 0)
                     {
+                        //I check if the string that I want to print has already something
                         if (current.Length > 0)
                         {
+                            //is is the case I will storage that and begin another to interate
                             sb.Append(current);
                             current = new StringBuilder();
                         }
 
+                        //I call the same function I finish the interator and the cald from inside to out
                         i = decompress(input, i + 1, current);
 
+                        //Now I know the times I do a loop to concat that
                         int repeatTimes = int.Parse(times.ToString());
                         for (int j = 0; j < repeatTimes; j++)
                         {
                             sb.Append(current);
                         }
 
+                        //here I just clean the variables to a new interation
                         current = new StringBuilder();
                         times = new StringBuilder();
                     }
+                    //Here a check when is close to begin another one or close the while
                     else if (input[i] == ']')
                     {
                         return i + 1;
                     }
+                    //Here I just know that i have to concat.
                     else
                     {
                         sb.Append(input[i]);
